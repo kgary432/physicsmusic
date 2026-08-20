ce4509e1e16154c1764db200cd348f7bff2d2dd6
kgary432 <kgary7373@gmail.com>
2026-08-20 13:04:42 -0400

    settingup token system

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // Public worker URL after you deploy worker/ with Wrangler.
 // Example: "https://physicsmusic-wall.yourname.workers.dev"
-window.PHYSICS_MUSIC_WORKER_URL = "";
+window.PHYSICS_MUSIC_WORKER_URL =
+  "https://physicsmusic-wall.uncaphysics102.workers.dev";
